e8eb58c868177c5de130dcbc7502badb86eb431d
myosookim <o_rul8_2@naver.com>
2025-12-24 12:32:25 +0900

    0001

---
+++ src/main/java/com/back/jpa/entity/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.back.jpa.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+    public String getModelTypeCode(){
+        return this.getClass().getSimpleName();
+    }
+}
