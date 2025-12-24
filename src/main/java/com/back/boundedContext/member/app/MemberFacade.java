c040cf683af0897d43038d3bc69dd932591ef79c
myosookim <o_rul8_2@naver.com>
2025-12-24 20:36:43 +0900

    0008

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -5,32 +5,34 @@ import com.back.global.exception.DomainException;
 import com.back.boundedContext.member.out.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberFacade {
     private final MemberRepository memberRepository;
+    private final MemberJoinUseCase memberJoinUserCase;
 
+    @Transactional(readOnly = true)
     public long count() {
         return memberRepository.count();
     }
 
+    @Transactional(readOnly = true)
     public Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
     }
 
+    @Transactional(readOnly = true)
     public Optional<Member> findById(int id) {
         return memberRepository.findById(id);
     }
 
+    @Transactional
     public Member join(String username, String password, String nickname) {
-        findByUsername(username).ifPresent(m -> {
-            throw new DomainException("409-1", "이미 존재하는 username 입니다.");
-        });
-
-        return memberRepository.save(new Member(username, password, nickname));
+        return memberJoinUserCase.join(username, password, nickname);
     }
 }
 
