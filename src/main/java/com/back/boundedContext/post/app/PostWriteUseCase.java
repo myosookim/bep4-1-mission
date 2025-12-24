e061c60a3f0b1e0f23c89abf575bdeb3cb490e42
myosookim <o_rul8_2@naver.com>
2025-12-25 02:48:27 +0900

    0014

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -6,6 +6,7 @@ import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.eventPublisher.EventPublisher;
 import com.back.global.rsData.RsData;
+import com.back.shared.member.out.MemberApiClient;
 import com.back.shared.post.dto.PostDto;
 import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
@@ -16,15 +17,17 @@ import org.springframework.stereotype.Service;
 public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
-    // 의도적인 결합도 발생
+    // 결합도 해결 (memberFacade -> memberApiClient)
     private final MemberFacade memberFacade;
+    private final MemberApiClient memberApiClient;
+
 
     public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
 
         eventPublisher.publish(new PostCreatedEvent(new PostDto(post)));
 
-        String randomSecureTip = memberFacade.getRandomSecureTip();
+        String randomSecureTip = memberApiClient.getRandomSecureTip();
 
         return new RsData<>("201-1", "%d번 글이 생성되었습니다. 보안 팁: %s".formatted(post.getId(),randomSecureTip),post);
     }
